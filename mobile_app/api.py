6196a8df239f767851e1bc1c6b4c7b7117e8f3aa
rayanaouf <rayanaouf1512@gmail.com>
2025-08-01 01:32:45 +0100

    add api file

---
+++ mobile_app/api.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+@frappe.whitelist(allow_guest=True)  # Or remove allow_guest for authenticated access only
+def hello_world(name=None):
+    return {
+        "message": f"Hello, {name or 'Guest'}!"
+    }
