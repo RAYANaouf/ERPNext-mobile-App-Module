e4cb8d22961eecc38f4cad00865fdf2bcce4c4fc
rayanaouf <rayanaouf1512@gmail.com>
2025-08-08 17:01:02 +0100

    add app nitification progresing ...

---
+++ mobile_app/mobile_app/doctype/mobile_notification/mobile_notification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ayanaouf and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mobile Notification", {
+// 	refresh(frm) {
+
+// 	},
+// });
